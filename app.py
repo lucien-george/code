2b49f5ca463e5965af10a9484f92c0d4aeb3c8e1
Lucien George <luciengeorge95@gmail.com>
2020-03-25 21:22:11 +0000

    test

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ from flask_restful import Api
 from restaurant_routes import Restaurants, RestaurantMember
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 api = Api(app)
 @app.before_first_request
 def create_tables():
   db.create_all()
 
-db.init_app(app)
 
 # @app.route('/')
 # def home():
@@ -19,6 +18,8 @@ db.init_app(app)
 
 api.add_resource(Restaurants, '/restaurants')
 api.add_resource(RestaurantMember, '/restaurants/<id>')
+
+db.init_app(app)
 
 if __name__ == "__main__":
   app.run(port=5000)
